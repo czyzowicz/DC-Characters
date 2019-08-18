407eacb18a4fceda1f3bd5fe934f5c723f4693ed
Michal Czyzowicz <czyzowicz.m@gmail.com>
2019-08-18 23:26:31 +0200

    subscirbe service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class HeroService {
-  getHeroes(): Hero[] {
-    return HEROES;
-  }
+  
   constructor() { }
+
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
 }
