255d31d16a243ed252883f9ca1a0d805e4b8c9d4
Michal Czyzowicz <czyzowicz.m@gmail.com>
2019-11-01 00:27:08 +0100

    add again dashboard

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,16 +1,14 @@
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable } from 'rxjs';
-import { of } from 'rxjs';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-
+@Injectable({ providedIn: 'root' })
 export class HeroService {
-  
+
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
@@ -18,6 +16,7 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
+
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
