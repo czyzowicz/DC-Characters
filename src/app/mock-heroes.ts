ca719a25e9e600aa305a2408dbc473947cb393f2
Michal Czyzowicz <czyzowicz.m@gmail.com>
2019-11-01 23:37:29 +0100

    changes in mock

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,24 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Batman', group: 'Heroes' },
-  { id: 12, name: 'Superman', group: 'Heroes'  },
-  { id: 13, name: 'Robin', group: 'Heroes' },
-  { id: 14, name: 'Joker', group: 'Villains' },
-  { id: 15, name: 'Two Faces', group: 'Villains' },
-  { id: 16, name: 'Ras al Ghul', group: 'Villains' },
-  { id: 17, name: 'Catwomen', group: 'Villains' },
-  { id: 18, name: 'Flash', group: 'Heroes' },
-  { id: 19, name: 'Green Lantern', group: 'Heroes' },
-  { id: 20, name: 'Wonder Woman', group: 'Heroes' }
+  { id: 1, name: 'Batman', group: 'Heroes' },
+  { id: 2, name: 'Superman', group: 'Heroes'  },
+  { id: 3, name: 'Robin', group: 'Heroes' },
+  { id: 4, name: 'Joker', group: 'Villains' },
+  { id: 5, name: 'Two Faces', group: 'Villains' },
+  { id: 6, name: 'Ras al Ghul', group: 'Villains' },
+  { id: 7, name: 'Catwomen', group: 'Villains' },
+  { id: 8, name: 'Flash', group: 'Heroes' },
+  { id: 9, name: 'Green Lantern', group: 'Heroes' },
+  { id: 10, name: 'Wonder Woman', group: 'Heroes' },
+  { id: 11, name: 'Nightwing', group: 'Heroes' },
+  { id: 12, name: 'Aquaman', group: 'Heroes'  },
+  { id: 13, name: 'Martian Manhunter', group: 'Heroes' },
+  { id: 14, name: 'The Penguin', group: 'Villains' },
+  { id: 15, name: 'Lex Luthor', group: 'Villains' },
+  { id: 16, name: 'Harley Quinn', group: 'Villains' },
+  { id: 17, name: 'Bane', group: 'Villains' },
+  { id: 18, name: 'John Constantine', group: 'Heroes' },
+  { id: 19, name: 'Cyborg', group: 'Heroes' },
+  { id: 20, name: 'Darkseid', group: 'Villains' }
 ];
