01001ab9f058a210b140ef2a67bfc225c29698f7
Michal Czyzowicz <czyzowicz.m@gmail.com>
2019-11-04 17:05:53 +0100

    delete mock heroes file

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,16 +5,22 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-characters';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService, private http: HttpClient
+  ) { }
 
-  getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  getHeroes (): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,25 +8,33 @@ import { Injectable } from '@angular/core';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Dr Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 1, name: 'Batman', group: 'Heroes' },
+      { id: 2, name: 'Superman', group: 'Heroes' },
+      { id: 3, name: 'Robin', group: 'Heroes' },
+      { id: 4, name: 'Joker', group: 'Villains' },
+      { id: 5, name: 'Two Faces', group: 'Villains' },
+      { id: 6, name: 'Ras al Ghul', group: 'Villains' },
+      { id: 7, name: 'Catwomen', group: 'Villains' },
+      { id: 8, name: 'Flash', group: 'Heroes' },
+      { id: 9, name: 'Green Lantern', group: 'Heroes' },
+      { id: 10, name: 'Wonder Woman', group: 'Heroes' },
+      { id: 11, name: 'Nightwing', group: 'Heroes' },
+      { id: 12, name: 'Aquaman', group: 'Heroes' },
+      { id: 13, name: 'Martian Manhunter', group: 'Heroes' },
+      { id: 14, name: 'The Penguin', group: 'Villains' },
+      { id: 15, name: 'Lex Luthor', group: 'Villains' },
+      { id: 16, name: 'Harley Quinn', group: 'Villains' },
+      { id: 17, name: 'Bane', group: 'Villains' },
+      { id: 18, name: 'John Constantine', group: 'Heroes' },
+      { id: 19, name: 'Cyborg', group: 'Heroes' },
+      { id: 20, name: 'Darkseid', group: 'Villains' }
     ];
-    return {heroes};
+    return { heroes };
   }
 
   // Overrides the genId method to ensure that a hero always has an id.
-  // If the heroes array is empty,
-  // the method below returns the initial number (11).
-  // if the heroes array is not empty, the method below returns the highest
-  // hero id + 1.
+  // If the heroes array is empty, the method below returns the initial number (1).
+  // if the heroes array is not empty, the method below returns the highest hero id + 1.
   genId(heroes: Hero[]): number {
     return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 1;
   }
