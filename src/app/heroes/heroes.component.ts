2b07024d105e70a7290ad1bf291754c8630a2717
Michal Czyzowicz <czyzowicz.m@gmail.com>
2019-11-06 14:31:51 +0100

    some changes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -22,7 +22,7 @@ export class HeroesComponent implements OnInit {
     .subscribe(heroes => this.heroes = heroes);
   }
 
-  add(name: string): void {
+  add(name: string, group: string): void {
     name = name.trim();
     if (!name) { return; }
     this.heroService.addHero({ name } as Hero)
